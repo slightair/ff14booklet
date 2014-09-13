f9383fa13c2babcb65c556c993d49a2dc1c5ad3b
slightair <arksutite@gmail.com>
2014-09-14 00:46:45 +0900

    Add WeatherForecastsViewController

---
+++ ff14booklet/TabBarItem.swift
@@ -0,0 +1,12 @@
+//
+//  TabBarController.swift
+//  ff14booklet
+//
+//  Created by slightair on 2014/09/13.
+//  Copyright (c) 2014年 slightair. All rights reserved.
+//
+
+import UIKit
+
+let TabBarItemIconFontSize: CGFloat = 30.0
+let TabBarItemIconImageSize = CGSizeMake(30.0, 30.0)
